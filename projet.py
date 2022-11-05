1aed69cd604e2ff6f0041e05f4ddd3b77cb52450
Mike Duran <mike.duran@hotmail.de>
2022-11-05 15:29:18 +0100

    functions added

--- projet.py
+++ projet.py
@@ -1,1 +1,20 @@
-# ici on écrit notre code.
+# ici on écrit notre code
+
+def afficher_10_premieres(dataframe):
+    return dataframe.head(10)
+
+def afficher_10_dernieres(dataframe):
+    return dataframe.head(-10)
+
+def getMean(dataframe):
+    return dataframe.mean()
+
+def getMin(dataframe):
+    return dataframe.min()
+
+def getMax(dataframe):
+    return dataframe.max()
+
+# 'Autres transports international' et 'Autres transports'
+def enlever_columns(dataframe):
+    return dataframe.drop(['Autres transports international','Autres transports'],axis=1).
