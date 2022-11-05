d7df97d02c473dfc82bb2eaf2688857bc936d593
Mike Duran <mike.duran@hotmail.de>
2022-11-05 15:51:45 +0100

    ajout de fichier csv ainsi que fin exo 3, 6 et 7

--- projet.py
+++ projet.py
@@ -1,6 +1,11 @@
 # ici on écrit notre code
 import pandas as pd
 
+raw_data=pd.read_csv("IGT - Pouvoir de réchauffement global - IGT - Pouvoir de réchauffement global.csv")
+
+#on enleve les Nan Values
+df=raw_data.dropna("index")
+
 def afficher_10_premieres(dataframe):
     return dataframe.head(10)
 
@@ -18,4 +23,25 @@ def getMax(dataframe):
 
 # 'Autres transports international' et 'Autres transports'
 def enlever_columns(dataframe):
-    return dataframe.drop(['Autres transports international','Autres transports'],axis=1).
+    return dataframe.drop(['Autres transports international','Autres transports'],axis=1)
+
+
+# afficher les 10 prem lignes
+print(afficher_10_premieres(df))
+
+# afficher les 10 dernières ligne
+print(afficher_10_dernieres(df))
+
+# afficher la moyenne
+print(getMean(df))
+
+# afficher les valeurs minimum
+print(getMin(df))
+
+#afficher les valeurs maximum
+print(getMax(df))
+
+#on enlevel les columns 'Autres transports international' et 'Autres transports'
+print(enlever_columns(df))
+
+
